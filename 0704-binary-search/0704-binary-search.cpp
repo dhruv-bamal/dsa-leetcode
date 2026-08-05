78641c84f05d228f4e4da2187b5a2538f8f9604d
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-05 13:31:31 +0530

    Time: 0 ms (100%), Space: 31.3 MB (47.06%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,14 +4,10 @@ public:
         int n = nums.size();
         int low = 0, high = n - 1;
         while(low <= high) {
-            int mid = (high + low)/2;
-            if(nums[mid] == target) {
-                return mid;
-            } else if(nums[mid] < target) {
-                low = mid + 1;
-            } else if(nums[mid] > target) {
-                high = mid - 1;
-            }
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
         }
         return -1;
     }
