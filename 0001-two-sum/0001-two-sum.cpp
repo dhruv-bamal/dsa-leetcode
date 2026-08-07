4ff2b3fc0afb5b1cc0e430efa5b5cbaebd117f79
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-08 02:13:51 +0530

    Time: 0 ms (100%), Space: 15 MB (23.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,9 +5,11 @@ public:
         unordered_map<int, int> mp;
         for(int i = 0; i < n; i++) {
             int complement = target - nums[i];
-            if(mp.find(complement) != mp.end()) return {mp[complement], i};
+            if(mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
             mp[nums[i]] = i;
         }
-        return {};
+        return {-1, -1};
     }
 };
