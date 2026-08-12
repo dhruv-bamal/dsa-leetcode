9348364654101268f081f658d5d846271f364397
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-13 02:46:06 +0530

    Time: 0 ms (100%), Space: 9.1 MB (13.81%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        unordered_map<char, char> mp = {
+            {')', '('},
+            {'}', '{'},
+            {']', '['}
+        };
+        for(char c : s) {
+            if(mp.find(c) != mp.end()) {
+                if(!stack.empty() && stack.top() == mp[c]) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.empty();
+    }
+};
