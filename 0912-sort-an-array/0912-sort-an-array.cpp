b500699d873448664833dbdb10cff6f70eda4d30
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-13 20:59:39 +0530

    Time: 728 ms (10.2%), Space: 337.2 MB (9.21%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,12 +1,43 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans;
-        for(int i = 0; i < n; i++) {
-            ans.push_back(nums[i]);
+    vector<int> merge(vector<int>& left, vector<int>& right) {
+        vector<int> res;
+        int i = 0, j = 0;
+        while (i < left.size() && j < right.size()) {
+            if (left[i] <= right[j]) {
+                res.push_back(left[i]);
+                i++;
+            } else {
+                res.push_back(right[j]);
+                j++;
+            }
         }
-        sort(ans.begin(), ans.end());
-        return ans;
+        while (i < left.size()) {
+            res.push_back(left[i]);
+            i++;
+        }
+        while (j < right.size()) {
+            res.push_back(right[j]);
+            j++;
+        }
+        return res;
+    }
+
+    vector<int> mergeSort(vector<int>& nums, int low, int high) {
+        if (low == high) {
+            return {nums[low]};
+        }
+        int mid = low + (high - low) / 2;
+        vector<int> left = mergeSort(nums, low, mid);
+        vector<int> right = mergeSort(nums, mid + 1, high);
+        vector<int> merged = merge(left, right);
+        return merged;
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return {};
+        }
+        return mergeSort(nums, 0, nums.size() - 1);
     }
 };
