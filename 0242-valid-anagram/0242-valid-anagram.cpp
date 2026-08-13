6230f127fda3a262843608e6c9c95acfb5f26a56
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-13 17:55:44 +0530

    Time: 3 ms (45.37%), Space: 9.7 MB (76.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
         vector<int> count(26, 0);
         for(int i = 0; i < s.length(); i++) {
             count[s[i] - 'a']++;
