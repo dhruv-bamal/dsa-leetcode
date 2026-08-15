a36099194e07f2ce5e71d5c527c17e2c11f6845e
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-15 23:29:30 +0530

    Time: 6 ms (12.18%), Space: 28.4 MB (14.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int, int> freq;
-        for(auto &it : nums) {
+        for(auto &it: nums) {
             freq[it]++;
         }
-        for(auto &it : freq) {
-            if(it.second > nums.size()/2) {
+        for(auto &it: freq) {
+            if(it.second > nums.size() / 2) {
                 return it.first;
             }
         }
-        return -1;
+        return 0;
     }
 };
