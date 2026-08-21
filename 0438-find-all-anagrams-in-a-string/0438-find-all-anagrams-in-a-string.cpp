262fc51a324883ef2a57693a78eb267d9a31c857
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-21 09:52:11 +0530

    Time: 4 ms (68.21%), Space: 11.6 MB (63.55%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -2,32 +2,28 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         int m = s.length(), n = p.length();
-        if (n > m) {
-            return {};
-        }
         vector<int> res;
-
+        if(n > m) {
+            return res;
+        }
         vector<int> countP(26, 0);
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             countP[p[i] - 'a']++;
         }
-
         vector<int> countS(26, 0);
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             countS[s[i] - 'a']++;
-        }
-        if (countS == countP) {
-            res.push_back(0);
-        }
-
-        for (int i = n; i < m; i++) {
-            countS[s[i] - 'a']++;
-            countS[s[i - n] - 'a']--;
-            if (countS == countP) {
+            if(countS == countP) {
                 res.push_back(i - n + 1);
             }
         }
-
+        for(int i = n; i < m; i++) {
+            countS[s[i] - 'a']++;
+            countS[s[i - n] - 'a']--;
+            if(countS == countP) {
+                res.push_back(i - n + 1);
+            }
+        }
         return res;
     }
 };
