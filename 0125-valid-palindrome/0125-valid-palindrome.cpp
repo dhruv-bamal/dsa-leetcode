dd1662c631f9b85c0715356a5f3db63353321415
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-24 05:19:49 +0530

    Time: 3 ms (28.2%), Space: 10.7 MB (8.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         string str = "";
-        for(char &c : s) {
-            if(isalnum(c)) {
+        for (char& c : s) {
+            if (isalnum(c)) {
                 c = tolower(c);
                 str += c;
             }
         }
         string rev = str;
         int left = 0, right = rev.length() - 1;
-        while(left <= right) {
+        while (left <= right) {
             swap(rev[left], rev[right]);
             left++;
             right--;
