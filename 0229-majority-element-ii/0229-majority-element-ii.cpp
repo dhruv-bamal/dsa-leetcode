28408acf6c69e8adb983e2c003aa3001bd092188
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-24 04:00:22 +0530

    Time: 11 ms (18.94%), Space: 26.6 MB (16.07%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        if(nums.empty()) {
-            return {};
-        }
-        unordered_map<int, int> freq;
-        for(auto &it: nums) {
-            freq[it]++;
+        unordered_map<int, int> hash;
+        for(auto &it : nums) {
+            hash[it]++;
         }
         vector<int> res;
-        for(auto &it: freq) {
+        for(auto &it : hash) {
             if(it.second > nums.size() / 3) {
                 res.push_back(it.first);
             }
