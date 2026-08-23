041d2617338bfcd3a717def18b06967d9e855871
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-24 05:19:39 +0530

    Time: 0 ms (100%), Space: 10.7 MB (8.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,7 +3,7 @@ public:
     bool isPalindrome(string s) {
         string str = "";
         for(char &c : s) {
-            if(isalnum(c)){
+            if(isalnum(c)) {
                 c = tolower(c);
                 str += c;
             }
