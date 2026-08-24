b579bebdf4d3b881cb4289e6f9c16fd869ca28b2
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-24 22:15:27 +0530

    Time: 27 ms (21.63%), Space: 17.4 MB (56.86%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,29 +2,27 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
+        if(n < 4) return {};
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
+        vector<vector<int>> res;
         for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
-            }
             for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                if (j > i + 1 && nums[j - 1] == nums[j])
                     continue;
-                }
                 int left = j + 1, right = n - 1;
                 while (left < right) {
                     long long sum =
                         1LL * nums[i] + nums[j] + nums[left] + nums[right];
                     if (sum == target) {
-                        ans.push_back(
+                        res.push_back(
                             {nums[i], nums[j], nums[left], nums[right]});
                         left++;
                         right--;
                         while (left < right && nums[left] == nums[left - 1])
                             left++;
-
-                        while (right > left && nums[right] == nums[right + 1])
+                        while (left < right && nums[right] == nums[right + 1])
                             right--;
                     } else if (sum < target) {
                         left++;
@@ -34,6 +32,6 @@ public:
                 }
             }
         }
-        return ans;
+        return res;
     }
 };
