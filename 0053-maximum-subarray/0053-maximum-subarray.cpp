9f37045defc9bb09df2a341e6c6d19c953d8f4fb
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-26 14:29:52 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.88%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -6,12 +6,8 @@ public:
         int res = INT_MIN, sum = 0;
         for(int i = 0; i < nums.size(); i++) {
             sum += nums[i];
-            if(sum > res) {
-                res = sum;
-            }
-            if(sum > 0) {
-                sum = 0;
-            }
+            res = max(res, sum);
+            if(sum < 0) sum = 0;
         }
         return res;
     }
