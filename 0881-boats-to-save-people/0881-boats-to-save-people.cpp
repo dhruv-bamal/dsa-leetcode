740c10c5020341675f7fe8c232170fb964902f29
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-27 05:54:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        int n = people.size();
+        int left = 0, right = people.size() - 1;
         sort(people.begin(), people.end());
-        vector<vector<int>> vec;
-        int left = 0, right = n - 1;
-        while (left <= right) {
-            if (people[left] + people[right] <= limit) {
-                vec.push_back({people[left], people[right]});
+        vector<vector<int>> res;
+        while(left < right) {
+            if(people[left] + people[right] < limit) {
+                res.push_back({people[left], people[right]});
                 left++;
                 right--;
             } else {
-                vec.push_back({people[right]});
+                res.push_back({people[right]});
                 right--;
             }
         }
-        return vec.size();
+        return res.size();
     }
 };
