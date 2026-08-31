916b9716b6a783dd0037ef082962bed841796b2a
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-31 13:03:06 +0530

    Time: 67 ms (71.53%), Space: 113.5 MB (15.95%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        return unordered_set<int>(nums.begin(), nums.end()).size() < nums.size();
+        return unordered_set<int>(nums.begin(), nums.end()).size() <
+               nums.size();
     }
 };
