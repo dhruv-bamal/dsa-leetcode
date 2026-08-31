aac6c232d8455e2b05ef40b38daf3e3b393be5a0
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-08-31 13:33:18 +0530

    Time: 127 ms (5%), Space: 80 MB (5.18%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        // Declare a set, declare a positive variable to 1, if the variable is present in the set increment it until missing positive is not found.
-        int n = nums.size(), firstPos = 1;
-        set<int> positive;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] > 0) {
-                positive.insert(nums[i]);
-            }
+        int firstPos = 1;
+        set<int> positives;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0)
+                positives.insert(nums[i]);
         }
-        for(int i = 0; i <= n + 1; i++) {
-            if(positive.find(firstPos) == positive.end()) {
+        for (int i = 0; i <= nums.size() + 1; i++) {
+            if (positives.find(firstPos) == positives.end())
                 return firstPos;
-            }
             firstPos++;
         }
-        return -1;
+        return 0;
     }
 };
