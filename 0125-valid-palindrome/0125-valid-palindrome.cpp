711b068f6bf0796f420dd828debf3b4cdc8d03e7
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-09-01 09:26:23 +0530

    Time: 0 ms (100%), Space: 9.9 MB (53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (!isalnum(s[i])) {
-                s.erase(i, 1);
-                i--;
-            } else {
-                s[i] = tolower(s[i]);
-            }
-        }
-        int low = 0, high = s.length() - 1;
-        while (low <= high) {
-            if (s[low] != s[high])
+        int low = 0, high = s.length();
+        while (low < high) {
+            while (low < high && !isalnum(s[low]))
+                low++;
+            while (low < high && !isalnum(s[high]))
+                high--;
+            if (tolower(s[low]) != tolower(s[high]))
                 return false;
             low++;
             high--;
