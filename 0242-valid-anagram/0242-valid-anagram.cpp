34a19d6d032ab2c8871c5369094bce9e8d99e9e2
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-09-16 23:32:18 +0530

    Time: 3 ms (44.99%), Space: 9.9 MB (11.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         unordered_map<char, int> countS, countT;
-        for (char& c : s)
-            countS[c]++;
-        for (char& c : t)
-            countT[c]++;
+        for (auto& it : s)
+            countS[it]++;
+        for (auto& it : t)
+            countT[it]++;
         return countS == countT;
     }
 };
