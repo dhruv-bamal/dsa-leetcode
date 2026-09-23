03e96abc00942160d272007e9efb7ca5e4508b07
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-09-24 01:40:23 +0530

    Time: 0 ms (100%), Space: 15 MB (12.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> hash;
-        for (int i = 0; i < nums.size(); i++) {
+        unordered_map<int, int> freq;
+        for(int i = 0; i < nums.size(); i++) {
             int missing = target - nums[i];
-            if (hash.count(missing))
-                return {hash[missing], i};
-            hash[nums[i]] = i;
+            if(freq.count(missing)) {
+                return {freq[missing], i};
+            }
+            freq[nums[i]] = i;
         }
         return {};
     }
