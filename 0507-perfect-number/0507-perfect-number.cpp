89b96910e053fe7c7004e13727f53f3c4599cc6d
Dhruv Bamal <bamaldhruv1105@gmail.com>
2026-09-24 20:05:14 +0530

    Time: 1048 ms (43.65%), Space: 7.7 MB (87.76%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        vector<int> divisors;
+        int sum = 0;
         for (int i = 1; i <= num / 2; i++) {
             if (num % i == 0)
-                divisors.push_back(i);
-        }
-        int sum = 0;
-        for (int i = 0; i < divisors.size(); i++) {
-            sum += divisors[i];
+                sum += i;
         }
         if (sum == num)
             return true;
